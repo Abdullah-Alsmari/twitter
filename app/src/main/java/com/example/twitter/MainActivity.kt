37b8643ccfcc582b5d566cce45671eaaf250bab9
A <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-09 02:16:23 +0530

    UI changes

--- app/src/main/java/com/example/twitter/MainActivity.kt
+++ app/src/main/java/com/example/twitter/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.twitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    var isLiked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val likeIcon = findViewById<ImageView>(R.id.img3)
+        likeIcon.setOnClickListener {
+            if(isLiked){
+                likeIcon.setImageResource(R.drawable.img5);
+            }else{
+                likeIcon.setImageResource(R.drawable.l5);
+            }
+
+            isLiked = !isLiked; // reverse
+
+        }
+
     }
 }
